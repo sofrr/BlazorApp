5bcdc78ecd52f7ec7402fb87a1d64c0fe75c4685
sof arg <sofarg@hotmail.com>
2021-03-26 20:37:27 +0200

    Complete todo functionality

--- BlazorApp/Shared/ToDoItem.cs
+++ BlazorApp/Shared/ToDoItem.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace BlazorApp.Shared
 {
-    public class ToDoItem
+    public class TodoItem
     {
         public string Title { get; set; }
         public bool IsDone { get; set; }
