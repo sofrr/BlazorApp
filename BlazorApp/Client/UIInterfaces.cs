9635209661b3fa3645b7dc010a6e7c1afd8c6581
sof arg <sofarg@hotmail.com>
2021-06-08 14:47:01 +0300

    Tab sets

---
+++ BlazorApp/Client/UIInterfaces.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.UIInterfaces
+{
+    public interface ITab
+    {
+        RenderFragment ChildContent { get; }
+    }
+}
